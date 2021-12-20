a5c5098d0e074b933edeb954af00c6e8e8c6498c
Darahaas N <65232365+Darahaas2001@users.noreply.github.com>
2021-12-21 00:54:44 +0530

    fixed Riddles

--- Routes/riddle.js
+++ Routes/riddle.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const RiddleController = require('../Controller/Riddles');
-const authController = require('../Controller/authController');
+//const authController = require('../Controller/authController');
 const Router = express.Router();
 
 Router.route('/')
-	.get(authController.protect, RiddleController.getAllRiddles)
-	.post(authController.protect, RiddleController.CreateRiddle);
+	.get(RiddleController.getAllRiddles)
+	.post(RiddleController.CreateRiddle);
+
 Router.route('/randomRiddle').get(RiddleController.getRandomRiddle);
+Router.route('/:id').patch(RiddleController.patchRiddle);
 
 module.exports = Router;
